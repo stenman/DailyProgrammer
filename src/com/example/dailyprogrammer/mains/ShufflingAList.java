cf821b6a483c6e97f6a1f4c4542a4f372b04eab2
stenman <roger.stenman@gmail.com>
2015-07-22 21:38:28 +0200

    First commit

---
+++ src/com/example/dailyprogrammer/mains/ShufflingAList.java
@@ -0,0 +1,10 @@
+package com.example.dailyprogrammer.mains;
+
+public class ShufflingAList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hej");
+	}
+
+}
